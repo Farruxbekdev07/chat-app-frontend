dcd22b80683b2776a876dd59ac97cb728b3a4db1
Farrukhbek <farrukhbekmirzakulov07@gmail.com>
2025-02-03 20:49:39 +0500

    setup project

---
+++ src/components/ChatWindow/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatWindow = () => {
+  return (
+    <div>
+      <h1>ChatWindow</h1>
+    </div>
+  );
+};
+
+export default ChatWindow;
---
+++ src/components/MessageBubble/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MessageBubble = () => {
+  return (
+    <div>
+      <h1>MessageBubble</h1>
+    </div>
+  );
+};
+
+export default MessageBubble;
